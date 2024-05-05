620d250e6adfd792896cde8b5950150383a94fb0
Trần Thanh Tư <mrturau@gmail.com>
2024-05-05 17:24:11 +0700

    Update environment.ts

--- Tutt_Client/src/environments/environment.ts
+++ Tutt_Client/src/environments/environment.ts
@@ -3,11 +3,9 @@ export const environment = {
     hmr: false,
     apiDomain: {
         EndPoint: 'https://localhost:7160',
-        FootballApi_SportsIo: 'https://v3.football.api-sports.io',
     },
 
     authenticationSettings: {
-        AuthorizationDatabase: 'Basic YWRtaW46YWJjMTIzLQ==',
-        RapidapiKey: '0d33439a7e97ab82c255dbfc73418a64'
+        AuthorizationDatabase: 'Basic YWRtaW46YWJ',
     },
 }
